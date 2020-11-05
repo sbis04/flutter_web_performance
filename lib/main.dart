00d55ab3926bd4cb3d9c7c6fe6d73a355dfdae62
Souvik Biswas <sbis1999@gmail.com>
2020-11-05 23:35:13 +0530

    Update main routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_web_performance/screens/about_page.dart';
+import 'package:flutter_web_performance/screens/description_page.dart';
 import 'package:flutter_web_performance/screens/home_page.dart';
 
 void main() {
@@ -10,15 +10,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Web Performance Testing',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: 'home',
-      routes: {
-        'home': (_) => HomePage(),
-        'about': (_) => AboutPage(),
+      onGenerateRoute: (settings) {
+        final arguments = settings.arguments;
+        switch (settings.name) {
+          case '/home':
+            return MaterialPageRoute(
+              builder: (context) => HomePage(),
+            );
+            break;
+          case '/description':
+            return MaterialPageRoute(
+              builder: (context) => DescriptionPage(arguments),
+            );
+            break;
+          default:
+            return MaterialPageRoute(
+              builder: (context) => HomePage(),
+            );
+            break;
+        }
       },
     );
   }
